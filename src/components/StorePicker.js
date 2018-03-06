5f2f1b0893bf0bc54f5f0ab2fd26ac89f3d06ff9
Ma'mun Mokni <mamun.mokni@gmail.com>
2018-03-06 19:57:57 +0100

    Initial commit

---
+++ src/components/StorePicker.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+class StorePicker extends Component {
+    render () {
+        return (
+            <React.Fragment>
+            <div>
+               <h2 className="header">Hello Nosnos</h2> 
+            </div>
+            <h2>Hello</h2>
+            </React.Fragment>
+        )
+    }
+}
+export default StorePicker
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+import StorePicker from './components/StorePicker';
+const main  = document.querySelector('#main')
+
+render(
+<div>
+<StorePicker />
+<StorePicker />
+</div>, 
+main)
